c4b030f922145966e41efb1ce2d9dc281b277a48
Mathieu Céraline <mathieu@crln.fr>
2021-07-16 17:56:35 -0400

    Adding back custom font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,12 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					{/* <link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
 					<link
 						href='https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,500&display=swap'
 						rel='stylesheet'
-					/> */}
+					/>
 				</Head>
 				<body>
 					<Main />
