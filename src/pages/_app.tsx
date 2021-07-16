7d6fbbfecb55020f809c79fa82f7f27d7af14b3c
Mathieu Céraline <mathieu@crln.fr>
2021-07-16 19:17:24 -0400

    Removing useEffect behavior

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import React from 'react';
 
-import useDarkMode from '@/hooks/useDarkMode';
+import { useDarkMode } from '@/hooks/useDarkMode';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	const [theme, setTheme] = useDarkMode();
 	const toggleTheme = () => {
-		setTheme('toggle');
+		if (theme === 'light') {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
 	};
 
 	return (
