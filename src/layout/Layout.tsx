146af8cf177557a69ca850b772e900e1693b2616
Mathieu Céraline <mathieu@crln.fr>
2021-07-16 22:58:54 -0400

    Temporarily removes custom hook for dark mode to prevent memory leak

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,7 @@
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// import { faBars, faTimes, faSun, faMoon, faMeteor, faMugHot, faNewspaper } from '@fortawesome/free-solid-svg-icons';
-
 import Header from './Header';
-// import { LayoutProps } from './types';
 import Footer from './Footer';
-// import Wrapper from './styles';
-
-// library.add(fab, faBars, faTimes, faSun, faMoon, faMeteor, faMugHot, faNewspaper);
 
 export default function Layout({ children }: { children: any }): JSX.Element {
-	// const [theme, toggleTheme] = useDarkMode();
-	// const themeMode = theme === 'light' ? lightTheme : darkTheme;
-
 	return (
 		<div className='flex flex-col min-h-full'>
 			<Header />
