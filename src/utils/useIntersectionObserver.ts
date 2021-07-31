e38583e12566f69a7bf039d7fe3eaf922e5c4906
Mathieu Céraline <mathieu@crln.fr>
2021-07-31 11:19:55 -0400

    Fix: IntersectionObserver issue on older devices - Try/Catch (attempt 2)

--- src/utils/useIntersectionObserver.ts
+++ src/utils/useIntersectionObserver.ts
@@ -7,45 +7,51 @@ interface HeadingRef {
 const useIntersectionObserver = (setActiveId: Dispatch<SetStateAction<string>>): void => {
 	const headingElementsRef = useRef<HeadingRef>({} as HeadingRef);
 	useEffect(() => {
-		const article = document.querySelector('article') || document;
+		try {
+			const article = document.querySelector('article') || document;
 
-		/**
-		 * This determines the zone for when an element is "visible".
-		 * */
-		const rootMarginValue = { rootMargin: '0px 0px -40% 0px' };
-		const headingElements = Array.from(article.querySelectorAll('h2, h3'));
+			/**
+			 * This determines the zone for when an element is "visible".
+			 * */
+			const rootMarginValue = { rootMargin: '0px 0px -40% 0px' };
+			const headingElements = Array.from(article.querySelectorAll('h2, h3'));
 
-		const callback = (headings: Array<IntersectionObserverEntry>) => {
-			headingElementsRef.current = headings.reduce((map, headingElement) => {
-				// eslint-disable-next-line no-param-reassign
-				map[headingElement.target.id] = headingElement;
-				return map;
-			}, headingElementsRef.current);
+			const callback = (headings: Array<IntersectionObserverEntry>) => {
+				headingElementsRef.current = headings.reduce((map, headingElement) => {
+					// eslint-disable-next-line no-param-reassign
+					map[headingElement.target.id] = headingElement;
+					return map;
+				}, headingElementsRef.current);
 
-			const visibleHeadings: Array<IntersectionObserverEntry> = [];
+				const visibleHeadings: Array<IntersectionObserverEntry> = [];
 
-			Object.keys(headingElementsRef.current).forEach((key) => {
-				const headingElement: IntersectionObserverEntry = headingElementsRef.current[key];
-				if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
-			});
+				Object.keys(headingElementsRef.current).forEach((key) => {
+					const headingElement: IntersectionObserverEntry = headingElementsRef.current[key];
+					if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
+				});
 
-			const getIndexFromId = (id: string) => headingElements?.findIndex((heading) => heading.id === id);
-			if (visibleHeadings.length > 0) {
-				setActiveId(visibleHeadings[0].target.id);
-			} else if (visibleHeadings.length > 1) {
-				const sortedVisibleHeadings = visibleHeadings.sort(
-					(a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id)
-				);
+				const getIndexFromId = (id: string) => headingElements?.findIndex((heading) => heading.id === id);
+				if (visibleHeadings.length > 0) {
+					setActiveId(visibleHeadings[0].target.id);
+				} else if (visibleHeadings.length > 1) {
+					const sortedVisibleHeadings = visibleHeadings.sort(
+						(a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id)
+					);
 
-				setActiveId(sortedVisibleHeadings[0].target.id);
-			}
-		};
+					setActiveId(sortedVisibleHeadings[0].target.id);
+				}
+			};
 
-		const observer = new IntersectionObserver(callback, rootMarginValue);
+			const observer = new IntersectionObserver(callback, rootMarginValue);
 
-		headingElements.forEach((element) => observer.observe(element));
+			headingElements.forEach((element) => observer.observe(element));
 
-		return () => observer.disconnect();
+			return () => observer.disconnect();
+		} catch (error) {
+			console.error(error);
+		}
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		return () => {};
 	}, [setActiveId]);
 };
 
