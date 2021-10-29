0e5c07756815736418f404b9168d72474d6b65d7
Mathieu Céraline <44712449+mathcrln@users.noreply.github.com>
2021-10-29 18:54:30 +0000

    Adds redirects based on old paths

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const withMDX = require('@next/mdx')();
 module.exports = withMDX({
 	reactStrictMode: true,
 	images: {
-		domains: ['res.cloudinary.com', 'source.unsplash.com', 'media.graphcms.com', 'localhost'],
+		domains: ['media.graphcms.com', 'localhost'],
+	},
+	async redirects() {
+		return [
+			{
+				source: '/lifestyle/:path',
+				destination: '/blog/:path',
+				permanent: true,
+			},
+		];
 	},
 });
