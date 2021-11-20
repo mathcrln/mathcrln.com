771daa11a0af539e385ac097293cc83d79151fc6
Mathieu Céraline <44712449+mathcrln@users.noreply.github.com>
2021-11-20 17:07:23 +0000

    Adds background on light mode STC

--- components/SkipToContent.tsx
+++ components/SkipToContent.tsx
@@ -1,7 +1,7 @@
 export default function SkipToContent(): JSX.Element {
 	return (
 		<a
-			className='px-4 py-2 max-w-max absolute top-0 left-2 visible text-center block w-full dark:bg-primary-dark transform -translate-y-10 focus:translate-y-4 duration-10 ease-in-out'
+			className='px-4 py-2 max-w-max absolute top-0 left-2 visible text-center block w-full dark:bg-primary-dark bg-white transform -translate-y-10 focus:translate-y-4 duration-10 ease-in-out'
 			href='#content'
 		>
 			Skip to content
